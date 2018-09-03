895c7158fd788e4c1cdf61e9d8875ef55c109245
Rreuben <jreuben07@gmail.com>
2018-09-03 22:03:34 +0300

    Defined the source and article class

--- app/models.py
+++ app/models.py
@@ -1,1 +1,31 @@
-from . import DB
+'''Module containing the source and article class'''
+
+class Source:
+
+    '''
+    Source class to define source objects
+    '''
+
+    def __init__(self, id, name, description, url, category):
+
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+
+
+class Article:
+
+    '''
+    Article class to define article objects
+    '''
+
+    def __init__(self, author, title, description, url, image_url, publish_time):
+
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.image_url = image_url
+        self.publish_time = publish_time
