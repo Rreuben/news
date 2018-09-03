36954119165c0671c8f024abb4e6436511493600
Rreuben <jreuben07@gmail.com>
2018-09-03 22:03:01 +0300

    Removed unnecessary code

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
+'''Package initialization module'''
+
 from flask import Flask
 from config import CONFIG_OPTIONS
 
 # Import extensions
 from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
 
 BOOTSTRAP = Bootstrap()
-DB = SQLAlchemy()
 
 def create_app(config_name):
 
@@ -17,9 +17,9 @@ def create_app(config_name):
 
     # Initializing flask extensions
     BOOTSTRAP.init_app(app)
-    DB.init_app(app)
 
     # Registering th blueprint
     from .main import MAIN as main_blueprint
     app.register_blueprint(main_blueprint)
+
     return app
