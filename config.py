7e0e3c02161347c76218d82109808db05f193b55
Rreuben <jreuben07@gmail.com>
2018-09-03 22:02:19 +0300

    Removed unnecessary code

--- config.py
+++ config.py
@@ -1,26 +1,32 @@
+'''Configuration classes module'''
+
 import os
 
 class Config:
+
     '''
     General configuration parent class
     '''
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://user:password@localhost/blog'
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SOURCE_URL = 'https://newsapi.org/v2/sources?language=en&apiKey={}'
+    ARTICLE_URL = 'https://newsapi.org/v2/everything?apiKey={}'
+    API_KEY = os.environ.get('API_KEY')
 
 
 class ProdConfig(Config):
+
     '''
     Production  configuration child class
 
     Args:
         Config: The parent configuration class with General configuration settings
     '''
+
     pass
 
 
 class DevConfig(Config):
+
     '''
     Development  configuration child class
 
@@ -29,6 +35,7 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+
 
 CONFIG_OPTIONS = {
     'development': DevConfig,
