c14b1702be257e8c78bc5e21b72f0508f1b0939c
Rreuben <jreuben07@gmail.com>
2018-09-04 13:27:27 +0300

    Added get_sources function to get json response from request

--- app/requests.py
+++ app/requests.py
@@ -30,7 +30,19 @@ def get_sources(category):
     Function that gets the json response to the url request
     '''
 
-    pass
+    get_sources_url = SOURCE_URL.format(category, API_KEY)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        sources_results = None
+
+        if get_sources_response['sources']:
+            sources_results_list = get_sources_response['sources']
+            sources_results = process_sources(sources_results_list)
+
+    return sources_results
 
 
 def process_sources(sources_list):
@@ -42,7 +54,19 @@ def process_sources(sources_list):
         sources_list: The list of dictionaries that contain source details
     '''
 
-    pass
+    sources_results = []
+
+    for item in sources_list:
+        id = item.get('id')
+        name = item.get('name')
+        description = item.get('description')
+        url = item.get('url')
+        category = item.get('category')
+
+        sources_object = Source(id, name, description, url, category)
+        sources_results.append(sources_object)
+
+    return sources_results
 
 
 def get_articles(id):
@@ -51,7 +75,19 @@ def get_articles(id):
     Function that gets the json response to the url request
     '''
 
-    pass
+    get_articles_url = ARTICLE_URL.format(id, API_KEY)
+
+    with urllib.request.urlopen(get_articles_url) as url:
+        articles_data = url.read()
+        article_response = json.loads(articles_data)
+
+        articles_results = None
+
+        if article_response['articles']:
+            article_results_list = article_response['articles']
+            articles_results = process_articles(article_results_list)
+
+    return articles_results
 
 
 def process_articles(article_list):
@@ -62,4 +98,20 @@ def process_articles(article_list):
     We process results that will transform our dictionary into a list of objects.
     '''
 
-    pass
+    article_results = []
+
+    for item in article_list:
+        author = item.get('author')
+        title = item.get('title')
+        description = item.get('description')
+        url = item.get('url')
+        image_url = item.get('urlToImage')
+        publish_time = item.get('publishedAt')
+
+        articles_object = Article(author, title, description, url, image_url, publish_time)
+        article_results.append(articles_object)
+
+    return article_results
+
+
+
